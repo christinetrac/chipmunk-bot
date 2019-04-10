8312560f781fa89abe39b0351f88ecc6a301d0a3
Christine Trac <43654021+christinetrac@users.noreply.github.com>
2019-04-09 20:41:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,73 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+    if (msg.content === 'how are you?') {
+        msg.reply('in a dog mood hbu');
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "hi") {
+        msg.reply('christine loves jonathan!!! heh')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "lol") {
+        msg.reply('do you need some lotion for your dryness? -__-')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "christine") {
+        msg.reply('im baby')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "jonathan") {
+        msg.reply('im doggy')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "ow") {
+        msg.reply('mark my words!! this is gona leave a bruise')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "i love you") {
+        msg.reply('christine told me to tell you she loves you too')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "bye") {
+        msg.reply('see u never... o__o')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "hehe") {
+        msg.reply('nyeah ehh')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "woof") {
+        msg.reply('malformation dog is dead :(')
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === "luv") {
+        msg.reply('want to sweet sweet?')
+    }
+});
 
 client.login('token'); //i cant put my actual token here anymore hahahah my bot got hacked so... :(
